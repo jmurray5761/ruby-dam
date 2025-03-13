b98bcb15b14731127511eb5088f6bd475d0ed1ed
Joseph Murray <joseph.murray@strongmind.com>
2025-03-13 10:56:36 -0700

    Fix delete functionality and ActiveStorage configuration

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -2,6 +2,5 @@
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-ActiveStorage::Analyzer::ImageAnalyzer::ImageMagick.analyzer = :image_processing
 
 Rails.application.config.active_storage.analyzers = [ActiveStorage::Analyzer::ImageAnalyzer::ImageMagick] 
